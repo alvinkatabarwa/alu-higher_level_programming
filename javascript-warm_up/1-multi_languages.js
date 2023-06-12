105c42fb12fd126960915072522f4b8059de0998
Alvin <a.katabarwa@alustudent.com>
2023-06-12 03:01:28 -0700

    finally

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-console.log(C is fun);
-console.log(Python is cool);
-console.log(JavaScript is amazing);
-=======
 #!/usr/bin/node
 console.log("C is fun");
 console.log("Python is cool");
 console.log("JavaScript is amazing");
->>>>>>> aaf5e6c46fbaf1b4d4784ea6153c373d6bfd89ba
