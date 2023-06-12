ed5836c8eb18bf03bc371617223b40a9395bc478
Alvin <a.katabarwa@alustudent.com>
2023-06-12 02:54:02 -0700

    heloo

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-let myVar;
-myVar = JavaScript is amazing
-=======
 #!/usr/bin/node
-let myVar;
-myVar = "JavaScript is amazing"
->>>>>>> aaf5e6c46fbaf1b4d4784ea6153c373d6bfd89ba
+let myVar = "JavaScript is amazing"
 console.log(myVar);
